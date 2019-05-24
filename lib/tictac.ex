4440b5a28a47324438a6113de464ffc94de29735
Gavin OMelia <gavinomelia@Gavins-MacBook-Pro.local>
2019-05-24 18:30:15 -0400

    initial commit

---
+++ lib/tictac.ex
@@ -0,0 +1,18 @@
+defmodule Tictac do
+  @moduledoc """
+  Documentation for Tictac.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ticta3c.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
